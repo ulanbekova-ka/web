3dbabb705d485367984e02939a7feb168ad1b596
kulanbekova <ulanbekova_ka@auca.kg>
2024-04-17 12:41:59 +0600

    Style pages

--- mysite/recommenderSystem/views.py
+++ mysite/recommenderSystem/views.py
@@ -21,9 +21,6 @@ def register_page(request):
 
 
 def login_page(request):
-    if request.user.is_authenticated:
-        return redirect('home')
-
     if request.method == "POST":
         username = request.POST.get('username')
         password = request.POST.get('password')
